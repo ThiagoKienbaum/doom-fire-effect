4754d131e10618eb8cc1a0f97132ecdfe3b98c32
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-04-26 17:12:47 -0300

    Add controls to fire effect

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+const fireColorsPallete = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
+const debugButton = document.querySelector('#debugButton').addEventListener('click', toogleDebugMode);
+const decreaseFireButton = document.querySelector('#decreaseFire').addEventListener('click', decreaseFire);
+const increaseFireButton = document.querySelector('#increaseFire').addEventListener('click', increaseFire);
+const minFireButton = document.querySelector('#minFire').addEventListener('click', minFire);
+const maxFireButton = document.querySelector('#maxFire').addEventListener('click', maxFire);
 const firePixelArray = [];
-const fireWidth = 10;
-const fireHeight = 10;
+var fireWidth = 40;
+var fireHeight = 40;
+var setFireIntensity = 36;
+var debug = false;
 
-start()
+start();
 
 function start() {
     createFireDataStructure();
     createFireSource();
-    renderFire();
-    setInterval(calculateFirePropagation, 1000);
+    renderFire();    
+    setInterval(calculateFirePropagation, 50);
 };
 
 function createFireDataStructure() {
@@ -23,11 +31,11 @@ function createFireSource() {
     for(let column = 0; column <= fireWidth; column++) {
         const overwritePixelIndex = fireWidth * fireHeight;
         const pixelIndex = (overwritePixelIndex - fireWidth) + column;
-        firePixelArray[pixelIndex] = 36;
+        firePixelArray[pixelIndex] = setFireIntensity;
     }
 }
 
-function renderFire() {
+function renderFire() {    
     let html = '<table cellpadding=0 cellspacing=0>';
 
     for(let row = 0; row < fireHeight; row++) {
@@ -35,10 +43,17 @@ function renderFire() {
         for(let column = 0; column < fireWidth; column++) {
             const pixelIndex = column + (fireWidth * row);
             const fireIntensity = firePixelArray[pixelIndex];
-            html += '<td>';
-            html += `<div class="pixel-index">${pixelIndex}</div>`;
-            html += fireIntensity
-            html += '</td>';
+            if(debug === true) {
+                html += '<td>';
+                html += `<div class="pixel-index">${pixelIndex}</div>`;
+                html += fireIntensity;
+                html += '</td>';
+            } else {
+                const color = fireColorsPallete[fireIntensity];
+                const colorString = `${color.r},${color.g},${color.b}`;
+                html += `<td class="pixel" style="background-color: rgb(${colorString})">`;
+                html += '</td>'
+            }
         }
         html += '</tr>';
     }
@@ -51,11 +66,55 @@ function calculateFirePropagation() {
         for(let row = 0; row < fireHeight; row++) {
             const pixelIndex = column + (fireWidth * row);
             updateFireIntensityPerPixel(pixelIndex);
-
         }
-    }
+    } 
+    renderFire();   
 };
 
 function updateFireIntensityPerPixel(currentPixelIndex) {
-    const belowPixelIndex = currentPixelIndex + fireWidth
-}
+    const belowPixelIndex = currentPixelIndex + fireWidth;
+
+    if(belowPixelIndex >= (fireWidth * fireHeight)) {
+        return;
+    }
+    const decay = Math.floor(Math.random() * 3);
+    const belowPixelFireIntensity = firePixelArray[belowPixelIndex];
+    const newFireIntensity = belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0;
+    firePixelArray[currentPixelIndex - decay] = newFireIntensity;    
+};
+
+function toogleDebugMode() {
+    if(debug === true) {
+        debug = false;
+        fireWidth = 40;
+        fireHeight = 40;
+    } else {        
+        debug = true;
+        fireWidth = 9;
+        fireHeight = 7;
+    }
+    createFireSource();
+};
+
+function decreaseFire() {
+    if(setFireIntensity > 0) {
+        setFireIntensity--;
+        createFireSource();
+    }
+};
+function increaseFire() {
+    if(setFireIntensity < 36) {
+        setFireIntensity++;
+        createFireSource();
+    }
+};
+
+function minFire() {
+    setFireIntensity = 0;
+    createFireSource();
+};
+
+function maxFire() {
+    setFireIntensity = 36;
+    createFireSource();
+};
